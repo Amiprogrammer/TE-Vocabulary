8cbc66192ad3c2851a0ed42467eec731e46a59f0
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-19 20:34:18 +0900

    Creating the window and other things

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from tkinter import *
+
+class App(Frame):
+
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.pack()
+        self.master.title("TE Vocabulary")
+        self.master.geometry("800x600")
+        self.config(bg="white")
+        self.master.config(bg="white")
+
+root = Tk()
+
+app = App(master=root)
+
+root.mainloop()
