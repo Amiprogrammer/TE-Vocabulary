4f9b0a2944f3444a506e5500772323d630ebc018
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-19 21:02:41 +0900

    Adding anything somethins is wrong :-(

--- main.py
+++ main.py
@@ -25,6 +25,26 @@ class App(Frame):
 
         self.master.config(menu=menubar)
 
+        toolbars = Frame(self)
+        toolbars["bg"] = "white"
+        toolbars.pack(side=TOP, fill=X)
+
+        photo = PhotoImage(file="img/file1.png")
+
+        photo_lbl = Label(toolbars, image=photo, width=100, height=100)
+        photo_lbl["bg"] = "white"
+        photo_lbl.photo = photo
+        photo_lbl.grid(row=0, column=0)
+
+        index_lbl = Label(toolbars, text="Tetum English Vocabulary.", bg="white", font=("Calibri",36))
+        index_lbl.grid(row=0, column=1)
+
+        find_lbl = Label(toolbars, text="Find your vocabulary with tetun keyword(s)!", anchor=W, justify=LEFT, bg="white", font=("Calibri",18))
+        find_lbl.grid(row=1, columnspan=2, pady=40)
+
+        self.user_entry = Entry(toolbars, font=("calibri",16))
+        self.user_entry.grid(row=2, column=0)
+
     def insert_date(self):
         pass
 
