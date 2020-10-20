b1e51a3a3eea80e5be352629fb1d117ce486bdc9
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-20 21:42:14 +0900

    Adding feature add a new date and change the background color of program

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ class App(Frame):
         self.pack()
         self.master.title("TE Vocabulary")
         self.master.geometry("730x560")
-        self.config(bg="white")
-        self.master.config(bg="white")
+        self.config(bg="grey")
+        self.master.config(bg="grey")
         self.all_here()
 
     def all_here(self):
@@ -37,30 +37,30 @@ class App(Frame):
         self.master.config(menu=menubar)
 
         toolbars1 = Frame(self)
-        toolbars1["bg"] = "white"
+        toolbars1["bg"] = "grey"
         toolbars1.pack(side=TOP, fill=X)
 
         photo = PhotoImage(file="img/file1.png")
 
         photo_lbl = Label(toolbars1, image=photo, width=100, height=100)
-        photo_lbl["bg"] = "white"
+        photo_lbl["bg"] = "grey"
         photo_lbl.photo = photo
         photo_lbl.grid(row=0, column=0)
 
-        index_lbl = Label(toolbars1, text="Tetum English Vocabulary.", bg="white", font=("Calibri",36))
+        index_lbl = Label(toolbars1, text="TE Vocabulary.", bg="grey", font=("Calibri",36))
         index_lbl.grid(row=0, column=1)
 
         toolbars2 = Frame(self)
-        toolbars2["bg"] = "white"
+        toolbars2["bg"] = "grey"
         toolbars2.pack(side=TOP, fill=X)
 
-        find_lbl = Label(toolbars2, text="Find your vocabulary with keyword(s) tetun !", anchor=W, justify=LEFT, bg="white", font=("Calibri Italic",16))
+        find_lbl = Label(toolbars2, text="Find your vocabulary with keyword(s) tetun !", anchor=W, justify=LEFT, bg="grey", font=("Calibri Italic",16))
         find_lbl.grid(row=0, pady=40)
 
-        toolbars3 = Frame(self, bg="white")
+        toolbars3 = Frame(self, bg="grey")
         toolbars3.pack(fill=X, side=TOP)
 
-        self.user_entry = Entry(toolbars3, justify=CENTER, bg="grey", font=("Calibri",16), width=28)
+        self.user_entry = Entry(toolbars3, justify=CENTER, font=("Calibri",16), width=28)
         self.user_entry.grid(row=0, column=0)
 
         right_arrow = PhotoImage(file="img/file2.png")
@@ -69,45 +69,72 @@ class App(Frame):
         confirm_button.right_arrow = right_arrow
         confirm_button.grid(row=0, column=1, padx=10)
 
-        toolbars4 = Frame(self, bg="white")
+        toolbars4 = Frame(self, bg="grey")
         toolbars4.pack(fill=X, side=TOP, pady=20)
 
-        tetun_index = Label(toolbars4, text="Tetun", font=("Calibri",24), bg="white")
+        tetun_index = Label(toolbars4, text="Tetun", font=("Calibri",24), bg="grey")
         tetun_index.grid(row=0, column=0)
 
-        english_index = Label(toolbars4, text="English", font=("Calibri",24), bg="white")
+        english_index = Label(toolbars4, text="English", font=("Calibri",24), bg="grey")
         english_index.grid(row=0, column=1, padx=60)
 
-        self.tetun = Label(toolbars4, text="", font=("Calibri",24), bg="white")
+        self.tetun = Label(toolbars4, text="", font=("Calibri",24), bg="grey")
         self.tetun.grid(row=1, column=0)
 
-        self.english = Label(toolbars4, text="", font=("Calibri",24), bg="white")
+        self.english = Label(toolbars4, text="", font=("Calibri",24), bg="grey")
         self.english.grid(row=1, column=1, padx=60)
 
     def insert_date(self):
-        pass
-
-    def about_TEV(self):
-        about = Toplevel()
-        about.title("About TE Vocabulary")
-        about.geometry("400x350")
+        insert_window = Toplevel()
+        insert_window.title("TE Vocabulary")
+        insert_window.geometry("480x350")
+        insert_window.config(bg="grey")
 
         photo = PhotoImage(file="img/file1.png")
 
-        te_img = Label(about, image=photo)
+        te_img = Label(insert_window, image=photo, text="Insert a new date!", compound=TOP, font=("Calibri",24), bg="grey")
         te_img.photo = photo
         te_img.pack()
 
-        self.te_lbl = Label(about, text="TE Vocabulary", font=("Calibri",24))
-        self.te_lbl.pack(pady=5)
+        form_insert = Frame(insert_window, bg="grey")
+        form_insert.pack()
+
+        tetun_lbl = Label(form_insert, text="Tetun", font=("Calibri",16), bg="grey")
+        tetun_lbl.grid(row=0, column=0)
+
+        english_lbl = Label(form_insert, text="English", font=("Calibri",16), bg="grey")
+        english_lbl.grid(row=0, column=1)
+
+        tetun_entry = Entry(form_insert, font=("Calibri",16), justify=CENTER)
+        tetun_entry.grid(row=1, column=0, padx=8)
+
+        english_entry = Entry(form_insert, font=("Calibri",16), justify=CENTER)
+        english_entry.grid(row=1, column=1, padx=8)
+
+        Button(form_insert, text="OK", font=("Cooper Black",12), bg="blue", fg="white", command=self.get_in_date).grid(row=2, columnspan=2, pady=20)
+
+    def get_in_date(self):
+        pass
+
+    def about_TEV(self):
+        about_window = Toplevel()
+        about_window.title("About TE Vocabulary")
+        about_window.geometry("400x350")
+        about_window.config(bg="grey")
+
+        photo = PhotoImage(file="img/file1.png")
+
+        self.te_lbl = Label(about_window, image=photo, text="TE Vocabulary", compound=TOP, font=("Calibri",24),bg="grey")
+        self.te_lbl.photo = photo
+        self.te_lbl.pack()
 
         var = StringVar()
         var.set("This program built with Python V3.8.2 programming languague and Tkinter V8.5 Module with associate MySQL database server to develop this program\n\n\n(C) 2020. <> by Juliao Martins")
 
-        message = Message(about, textvariable=var, font=("Calibri Bold",8), width=180, justify=CENTER)
+        message = Message(about_window, textvariable=var, font=("Calibri Bold",8), width=180, justify=CENTER, bg="grey", fg="white")
         message.pack(pady=10)
 
-        Button(about, text="Dismiss", bg="red", fg="black", command=about.destroy).pack()
+        Button(about_window, text="Dismiss", bg="red", fg="black", command=about_window.destroy).pack()
 
         self.te_lbl.bind("<Enter>",self.change_color)
         self.te_lbl.bind("<Leave>",self.default_color)
@@ -126,6 +153,10 @@ class App(Frame):
             if( len(input) == 0 ):
                 messagebox.showwarning("TE Vocabulary",
                                 "Please Insert Something!")
+
+                self.tetun.config(text="", fg="black")
+                self.english.config(text="", fg="black")
+
             elif( len(input) > 0 ):
                 sql = "SELECT tetun,english FROM words WHERE tetun = %s"
                 val = (input.lower(),)
