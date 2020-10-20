40f86929db8de69e36cc7bc9d680f31cbec583cf
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-20 13:24:52 +0900

    Adding the feature GUI interface is done! now focuslly to database connected and logical the program

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class App(Frame):
         super().__init__(master)
         self.pack()
         self.master.title("TE Vocabulary")
-        self.master.geometry("800x600")
+        self.master.geometry("730x560")
         self.config(bg="white")
         self.master.config(bg="white")
         self.all_here()
@@ -25,25 +25,44 @@ class App(Frame):
 
         self.master.config(menu=menubar)
 
-        toolbars = Frame(self)
-        toolbars["bg"] = "white"
-        toolbars.pack(side=TOP, fill=X)
+        toolbars1 = Frame(self)
+        toolbars1["bg"] = "white"
+        toolbars1.pack(side=TOP, fill=X)
 
         photo = PhotoImage(file="img/file1.png")
 
-        photo_lbl = Label(toolbars, image=photo, width=100, height=100)
+        photo_lbl = Label(toolbars1, image=photo, width=100, height=100)
         photo_lbl["bg"] = "white"
         photo_lbl.photo = photo
         photo_lbl.grid(row=0, column=0)
 
-        index_lbl = Label(toolbars, text="Tetum English Vocabulary.", bg="white", font=("Calibri",36))
+        index_lbl = Label(toolbars1, text="Tetum English Vocabulary.", bg="white", font=("Calibri",36))
         index_lbl.grid(row=0, column=1)
 
-        find_lbl = Label(toolbars, text="Find your vocabulary with tetun keyword(s)!", anchor=W, justify=LEFT, bg="white", font=("Calibri",18))
-        find_lbl.grid(row=1, columnspan=2, pady=40)
+        toolbars2 = Frame(self)
+        toolbars2["bg"] = "white"
+        toolbars2.pack(side=TOP, fill=X)
 
-        self.user_entry = Entry(toolbars, font=("calibri",16))
-        self.user_entry.grid(row=2, column=0)
+        find_lbl = Label(toolbars2, text="Find your vocabulary with keyword(s) tetun !", anchor=W, justify=LEFT, bg="white", font=("Calibri",18))
+        find_lbl.grid(row=0, pady=40)
+
+        toolbars3 = Frame(self, bg="white")
+        toolbars3.pack(fill=X, side=TOP)
+
+        self.user_entry = Entry(toolbars3, font=("calibri",16), width=32, bg="grey", justify=CENTER)
+        self.user_entry.grid(row=1, column=0)
+
+        go_img = PhotoImage(file="img/file2.png")
+
+        ok = Button(toolbars3, image=go_img, compound=CENTER, width=45, height=35, command=self.destroy)
+        ok.go_img = go_img
+        ok.grid(row=1, column=1, padx=20)
+
+        result_img = PhotoImage(file=r"img\file3.png")
+
+        self.results = Label(toolbars3, text="Result Here!", font=("Calibri",36), image=result_img, compound=TOP, bg="white", width=250, height=160)
+        self.results.result_img = result_img
+        self.results.grid(row=2, columnspan=2, pady=100)
 
     def insert_date(self):
         pass
